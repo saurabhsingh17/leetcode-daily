c8588e927b015b3be89f787b8069b7247cf06b33
Saurabh Singh <50754992+saurabhsingh17@users.noreply.github.com>
2024-04-13 21:36:31 +0530

    Time: 194 ms (5.04%), Space: 59.1 MB (13.13%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,30 +1,24 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int[] nsr = nextSmallerElement(heights);
-        int[] nsl = nextSmallerElementToLeft(heights);
-        int max = 0;
-        for (int i = 0; i < heights.length; i++) {
-            int sum = heights[i] * (nsr[i] - nsl[i] - 1);
-            if (sum > max) {
-                max = sum;
-            }
-        }
-        return max;
+        return areaHistogram(heights,nextSmallerElementToLeft(heights),nextSmallerRight(heights));
     }
-    static int[] nextSmallerElement(int[] nums) {
-        int[] nsr = new int[nums.length];
+    static int areaHistogram(int[] nums, int[] nsl, int[] nsr) {
         int n = nums.length;
-        Stack<int[]> stack = new Stack<>();
-
-        for (int i = n - 1; i >= 0; i--) {
-            int num = nums[i];
-            while (!stack.empty() && stack.peek()[0] >= nums[i]) stack.pop();
-            nsr[i] = stack.isEmpty() ? n : stack.peek()[1];
-            stack.push(new int[]{num, i});
+        int width[] = new int[n];
+        int area[] = new int[n];
+        int maxArea = 0;
+        for(int i = 0; i < n; i++) {
+            
+            width[i] = nsr[i] - nsl[i] - 1;
+            area[i] = width[i] * nums[i];
+            if(area[i] > maxArea) maxArea = area[i];
         }
-        return nsr;
-    }
 
+        System.out.println(Arrays.toString(width));
+        System.out.println(Arrays.toString(area));
+        return maxArea;
+
+    }
     static int[] nextSmallerElementToLeft(int[] nums) {
         int[] nsl = new int[nums.length];
         int n = nums.length;
@@ -37,5 +31,18 @@ class Solution {
             stack.push(new int[]{num, i});
         }
         return nsl;
+    }
+    static int[] nextSmallerRight(int[] nums) {
+        int[] nsr = new int[nums.length];
+        int n = nums.length;
+        Stack<int[]> stack = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            int num = nums[i];
+            while (!stack.empty() && stack.peek()[0] >= nums[i]) stack.pop();
+            nsr[i] = stack.isEmpty() ? n : stack.peek()[1];
+            stack.push(new int[]{num, i});
+        }
+        return nsr;
     }
 }
