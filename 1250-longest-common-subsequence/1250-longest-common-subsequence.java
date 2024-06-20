cb00dff3a25c1378f3f1075b67f8ceedc54b42e4
Saurabh Singh <50754992+saurabhsingh17@users.noreply.github.com>
2024-06-21 00:26:24 +0530

    Time: 20 ms (74.67%), Space: 51 MB (18%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -14,11 +14,6 @@ class Solution {
     static int solve(char[] t1, int m, char[] t2, int n,int[][] dp){
         if (n == 0 || m == 0) return 0;
         if (dp[m][n] != -1) return dp[m][n];
-        if(t1[m-1] == t2[n-1]) {
-            return dp[m][n] = 1 + solve(t1,m-1,t2,n-1,dp);
-        }
-        else {
-            return dp[m][n] = Math.max(solve(t1,m-1,t2,n,dp),solve(t1,m,t2,n-1,dp));
-        }
+        return dp[m][n] = (t1[m-1] == t2[n-1]) ? 1 + solve(t1,m-1,t2,n-1,dp): Math.max(solve(t1,m-1,t2,n,dp),solve(t1,m,t2,n-1,dp));
     }
 }
