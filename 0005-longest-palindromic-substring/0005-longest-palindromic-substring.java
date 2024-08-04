4104b127221ce43899898801cc14cf64846f6454
Saurabh Singh <50754992+saurabhsingh17@users.noreply.github.com>
2024-08-04 12:50:08 +0530

    Time: 16 ms (77.01%), Space: 45.2 MB (40.37%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,55 +1,21 @@
 class Solution {
     public String longestPalindrome(String str) {
-        if (str == null || str.length() == 0) {
-            return "";
-        }
-
-        String processedStr = preprocess(str);
-        int n = processedStr.length();
-        int[] p = new int[n];
-        int center = 0, right = 0;
+        int n = str.length();
+        String res = "";
         
         for (int i = 0; i < n; i++) {
-            int mirror = 2 * center - i;
-            if (right > i) {
-                p[i] = Math.min(right - i, p[mirror]);
-            }
-
-            int a = i + p[i] + 1;
-            int b = i - p[i] - 1;
-            while (a < n && b >= 0 && processedStr.charAt(a) == processedStr.charAt(b)) {
-                p[i]++;
-                a++;
-                b--;
-            }
-
-            if (i + p[i] > right) {
-                center = i;
-                right = i + p[i];
-            }
+            res = getLongerPalindrome(str, i, i, res);     // Odd-length palindromes
+            res = getLongerPalindrome(str, i, i + 1, res); // Even-length palindromes
         }
-
-        int maxLen = 0;
-        int centerIndex = 0;
-        for (int i = 0; i < n; i++) {
-            if (p[i] > maxLen) {
-                maxLen = p[i];
-                centerIndex = i;
-            }
-        }
-
-        int start = (centerIndex - maxLen) / 2;
-        return str.substring(start, start + maxLen);
+        return res;
     }
 
-    private String preprocess(String str) {
-        StringBuilder sb = new StringBuilder();
-        sb.append('#');
-        for (char c : str.toCharArray()) {
-            sb.append(c);
-            sb.append('#');
+    private String getLongerPalindrome(String str, int left, int right, String currentLongest) {
+        while (left >= 0 && right < str.length() && str.charAt(left) == str.charAt(right)) {
+            left--;
+            right++;
         }
-        return sb.toString();
+        String palindrome = str.substring(left + 1, right);
+        return palindrome.length() > currentLongest.length() ? palindrome : currentLongest;
     }
 }
-
